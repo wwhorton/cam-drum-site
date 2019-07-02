6367c5f361cd923276f9f3f0faba4430c5ab0a42
Bill Whorton <wwhorton@chesapeakebay.net>
2019-07-02 14:29:26 -0400

    Adds new model

--- cam_drum_site/admin.py
+++ cam_drum_site/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 from .models import Article, ArticleAdmin, Card
 
 # Register your models here.
-admin.site.register(Article, ArticleAdmin, Card)
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Card)
